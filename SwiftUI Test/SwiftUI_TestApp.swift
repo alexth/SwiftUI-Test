8ddfe648a60f1119db784d215486efc0ca390ff4
Alex Golub <alexeythumb@gmail.com>
2021-11-23 23:23:18 +0200

    Add project

---
+++ SwiftUI Test/SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestApp.swift
+//  SwiftUI Test
+//
+//  Created by Alex Golub on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
